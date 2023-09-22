9e771ca4711ebc12360e3726dcb2d126844e39fe
dhihanaa <dhihanaagustinakolimah@gmail.com>
2023-09-22 15:29:17 +0700

    upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:crud_buku/Books/list_book.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CRUD API Buku',
+      home: BookList(),
+    );
+  }
+}
